1c48dec04e4afdab189dd6f02124d2fa564ed4ed
fire8327 <fire83274@gmail.com>
2025-10-03 18:20:18 +0300

    create footer

---
+++ app/components/Footer.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="w-full grid-container pb-10 xl:pb-20 mt-auto">
+        <div class="flex max-sm:flex-col gap-2 sm:gap-10 justify-between">
+            <div class="flex flex-col gap-3">
+                <a href="/">
+                    <img src="/images/header/logo.png" alt="" class="w-[109px] sm:w-[158px]">
+                </a>
+                <p class="max-sm:hidden text-[#ADADAD] w-full sm:max-w-[330px] md:max-w-[420px] xl:max-w-[510px] text-[8px] sm:text-[11px] leading-[13px] sm:leading-[21px]">
+                    Мы используем cookies для сбора анонимных данных о пользователях.  Эти данные помогают настраивать рекламу  и анализировать трафик. Продолжая использовать наш сайт, вы соглашаетесь на сбор и обработку таких данных.
+                </p>
+            </div>
+            <div class="flex max-sm:flex-col gap-2 sm:gap-6 xl:gap-[105px]">
+                <div class="grid grid-cols-2 sm:grid-cols-1 max-sm:gap-x-5 gap-y-4 text-[12px] sm:text-[15px] leading-[100%]">
+                    <a href="/">Как пользоваться</a>
+                    <a href="/">Тарифы</a>
+                    <a href="/">Помощь</a>
+                    <a href="/">Договор-оферта</a>
+                </div>
+                <div class="flex sm:flex-col max-sm:items-center gap-9 sm:gap-4">
+                    <button class="w-fit px-4 py-1.5 rounded-full border border-[#136BFB] text-[#136BFB] font-semibold text-[12px] sm:text-[16px] leading-[100%]">Попробовать</button>
+                    <div class="flex items-center gap-2">
+                        <a href="/">
+                            <img src="/images/footer/vk.svg" alt="" class="w-7 sm:w-9">
+                        </a>
+                        <a href="/">
+                            <img src="/images/footer/tg.svg" alt="" class="w-7 sm:w-9">
+                        </a>
+                    </div>
+                </div>
+                <p class="mt-0.5 sm:hidden text-[#ADADAD] w-full sm:max-w-[330px] md:max-w-[420px] xl:max-w-[510px] text-[8px] sm:text-[11px] leading-[13px] sm:leading-[21px]">
+                    Мы используем cookies для сбора анонимных данных о пользователях.  Эти данные помогают настраивать рекламу  и анализировать трафик. Продолжая использовать наш сайт, вы соглашаетесь на сбор и обработку таких данных.
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -3,4 +3,5 @@
     <main class="grid-container grow gap-y-[50px] md:gap-y-[70px] mb-[50px] md:mb-[70px] xl:mb-20">
         <slot/>
     </main>
+    <Footer></Footer>
 </template>
