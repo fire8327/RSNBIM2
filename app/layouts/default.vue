87f6f6364672a32ae99b9cfb8b95d02fa6c0df03
fire8327 <fire83274@gmail.com>
2025-10-05 14:12:52 +0300

    upd hero

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
     <Header></Header>
-    <main class="grid-container grow gap-y-[50px] md:gap-y-[70px] mt-7 xl:mt-16 mb-[50px] md:mb-[70px] xl:mb-20">
+    <main class="grid-container grow gap-y-[50px] md:gap-y-[70px] mt-7 xl:mt-16 mb-[50px] md:mb-[70px] xl:mb-20 overflow-x-clip">
         <slot/>
     </main>
     <Footer></Footer>
