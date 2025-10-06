803f79d21610842aca51d533263c527ceea22fa3
fire8327 <fire83274@gmail.com>
2025-10-07 01:25:40 +0300

    upd

--- app/pages/contacts.vue
+++ app/pages/contacts.vue
@@ -1,4 +1,25 @@
 <template>
+    <div class="w-full col-span-full grid-container bg-gradient-to-b from-[#136BFB] to-[#6E99E1] py-9 md:py-14 xl:py-[60px]">
+        <div class="flex flex-col gap-10 xl:gap-11">
+            <div class="grid grid-cols-3 gap-3.5 xl:gap-5 text-[17px] xl:text-[19px] leading-[25px] font-medium max-md:hidden">
+                <div class="rounded-[20px] bg-white flex items-center gap-4 xl:gap-7 py-6 xl:py-7 px-5 xl:px-10">
+                    <img src="/images/contacts/phone.svg" alt="" class="w-6">
+                    <a target="_blank" href="tel:+74957418963">+7 (495) 741-89-63</a>
+                </div>
+                <div class="rounded-[20px] bg-white flex items-center gap-4 xl:gap-7 py-6 xl:py-7 px-5 xl:px-10">
+                    <img src="/images/contacts/mail.svg" alt="" class="w-6">
+                    <div class="relative">
+                        <a target="_blank" href="support@rsnbim.ru">support@rsnbim.ru</a>
+                        <span class="text-[12px] leading-[100%] text-[#ADADAD] font-medium absolute left-0 -top-2.5">Тех. поддержка</span>
+                    </div>
+                </div>
+                <div class="rounded-[20px] bg-white flex items-center gap-4 xl:gap-7 py-6 xl:py-7 px-5 xl:px-10">
+                    <img src="/images/contacts/calendar.svg" alt="" class="w-[22px]">
+                    <span>Пн-Пт с 09:00 до 17:00</span>
+                </div>
+            </div>
+        </div>
+    </div>
     <div class="flex flex-col gap-10 md:gap-11 xl:gap-9">
         <div class="text-[12px] md:text-[15px] leading-[160%] flex max-md:flex-col gap-1.5 sm:gap-5 md:gap-10 md:justify-between w-full px-3.5 py-4 md:px-9 md:py-5 xl:px-[100px] bg-[#F7F7F7] rounded-[17.5px] md:rounded-[20px]">
             <div class="flex max-md:flex-col md:gap-3 xl:gap-6">
