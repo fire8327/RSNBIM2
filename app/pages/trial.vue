785a7af271bcbd7d61fbe30027d63508c05fa7df
fire8327 <fire83274@gmail.com>
2025-10-08 21:54:15 +0300

    create trial page

---
+++ app/pages/trial.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="flex max-md:flex-col gap-10 md:gap-6 md:justify-between items-center">
+        <p class="text-[25px] md:text-[38px] xl:text-[40px] leading-[130%] font-medium">
+            <span class="inline-flex gap-1 md:gap-4 whitespace-nowrap">Добро пожаловать <img src="/images/trial/hand.svg" alt="" class="w-6 md:w-12"></span> <br> 
+            на страницу пробного тестирования облачного сервера Revit!
+        </p>
+        <img src="/images/trial/hero.png" alt="" class="w-full md:w-[360px] xl:w-[534px]">
+    </div>
+    <div class="flex flex-col gap-9 md:gap-12 xl:gap-9">
+        <div class="flex flex-col items-center text-center gap-0.5 md:gap-2">
+            <p class="text-[12px] md:text-[17px] leading-[100%] font-medium text-[#136BFB]">Как подключиться?</p>
+            <p class="text-[23px] md:text-[35px] leading-[120%] md:leading-[100%] font-medium">Серверы для подключения</p>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
